2e7bda87ef4f4068048e455925a454378f17f665
James <IdiosApps@users.noreply.github.com>
2019-10-06 15:13:11 +0100

    Set app name in title bar

--- SoundboardForm.cs
+++ SoundboardForm.cs
@@ -30,6 +30,7 @@ namespace IdiosAppsSoundboard
             Width = Screen.PrimaryScreen.Bounds.Width / 2;
             buttonPadding = Convert.ToInt32(Width * 0.02);
             titlebarHeight = Height - ClientSize.Height;
+            Text = "IdiosApps' dynamic soundbar";
         }
 
         private void SoundboardForm_Load(object sender, EventArgs e)
