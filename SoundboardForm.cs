7aa4ab39248a778f7412d3e3c57b12126609330e
James <IdiosApps@users.noreply.github.com>
2019-10-06 01:13:53 +0100

    Ensure no button overlaps

--- SoundboardForm.cs
+++ SoundboardForm.cs
@@ -14,6 +14,10 @@ namespace IdiosAppsSoundboard
         private readonly List<string> audioExtensions = new List<string>() {".mp3", ".wav"};
         private WindowsMediaPlayer soundPlayer = new WindowsMediaPlayer();
         private readonly string appDirectory = Directory.GetCurrentDirectory();
+        private int buttonWidth = 100;
+        private int buttonHeight = 100;
+        private Random random = new Random();
+
         public SoundboardForm()
         {
             InitializeComponent();
@@ -37,7 +41,6 @@ namespace IdiosAppsSoundboard
 
         private void generateAudioButtons(List<FileInfo> files)
         {
-            var random = new Random();
             foreach (var file in files)
             {
                 // TODO Use number of loaded files (limited?) to fill out a fixed-size window (e.g. 4x5)
@@ -45,8 +48,8 @@ namespace IdiosAppsSoundboard
                 {
                     Name = file.Name,
                     Text = Path.GetFileNameWithoutExtension(file.Name),
-                    Size = new Size(100, 100),
-                    Location = new Point(random.Next(0, this.Width - 100), random.Next(0, this.Height - 100)),
+                    Size = new Size(buttonWidth, buttonHeight),
+                    Location = generateNonOverlappingPosition(),
                     BackColor = Color.DimGray,
                     ForeColor = Color.GhostWhite,
                     FlatStyle = FlatStyle.Flat,
@@ -72,6 +75,27 @@ namespace IdiosAppsSoundboard
             var button = sender as Button;
             soundPlayer.URL = Path.Combine(appDirectory, button.Name);
             soundPlayer.controls.play();
+        }
+
+        private Point generateNonOverlappingPosition()
+        {
+            while (true) // To be dynamic, would have to adjust size (& font) for large # of files
+            {
+                bool newButtonOverlaps = false;
+
+                Point location = new Point(random.Next(0, this.Width - buttonWidth), random.Next(0, this.Height - buttonHeight));
+                Size size = new Size(buttonWidth, buttonHeight);
+                Rectangle rectangle = new Rectangle(location, size);
+
+                foreach (var button in Controls.OfType<Button>())
+                {
+                    if (button.Bounds.IntersectsWith(rectangle))
+                        newButtonOverlaps = true;
+                }
+
+                if (!newButtonOverlaps)
+                    return location;
+            }
         }
     }
 }
